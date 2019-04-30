3134ff7bc7c135c2306721d80dea2fe7bf965dbe
LienaPuey <lienapuey@gmail.com>
2019-04-30 11:49:35 +0200

    demo_update

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ let claves = Object.keys(datos)
     for (var j = 0; j < datos[claves[i]].length; j++) {
       console.log(datos[claves[i]][j]);
      cardBody.innerHTML +=
-          `  <div class="col-lg-4">
+          `
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140">
           <rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text>
         </svg>
         <h2>${datos[claves[i]][j]["name"]}</h2>
         <p>${datos[claves[i]][j]["missions"][0] != undefined ? datos[claves[i]][j]["missions"][0]["description"] : "Mission undefined"}</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->`
+      `
       }
     }
 
